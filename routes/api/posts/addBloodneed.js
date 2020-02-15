260849bce16cf6d2757ce94f4b2ad680f92c9025
Tharindu Basanayaka <tbasnayaka7@gmail.com>
2020-02-16 00:36:01 +0530

    add blood need posts

---
+++ routes/api/posts/addBloodneed.js
@@ -0,0 +1,27 @@
+var admin = require("firebase-admin");
+var express = require("express");
+var moment = require("moment");
+
+var db = admin.firestore();
+
+const addbloodposts = express.Router();
+routeraddbloodneed.post("/", function(req, res) {
+  db.collection("posts-blood_needed")
+    .add({
+      userID: req.body.userID,
+      userName: req.body.userName,
+      contactNo: req.body.phoneNumber,
+      bloodType: req.body.bloodType,
+      imgSrc: req.body.imgSrc,
+      createdAt: req.body.publishedDateTimeAgo
+    })
+    .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+      res.send(200, "Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+      console.error("Error adding document: ", error);
+    });
+});
+
+module.exports = routeraddbloodneed;
