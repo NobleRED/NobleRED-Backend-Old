2395e58ff7af73cea219d46c912e252a3152ee0f
Sulakshana <sula.ranawake@gmail.com>
2020-02-15 16:44:29 +0530

    api updated

--- index.js
+++ index.js
@@ -67,102 +67,24 @@ const newCampaignReq = require('./routes/api/campaigns/insertNew');
 app.use('/api/campaigns/new', newCampaignReq);
 
 // accepting a new campaign request to the db
-const acceptReq = require('./routes/api/campaigns/acceptReq');
-app.use('/api/campaigns/requests', acceptReq);
+// const acceptReq = require('./routes/api/campaigns/acceptReq');
+// app.use('/api/campaigns/accept/:cid', acceptReq);
+app.post('/api/campaigns/accept/:cid', function (req, res) {
+    const cid = req.params.cid
 
+    const docRef = db.collection("campaigns-requests").where("campaignID", '==', cid);
+    db.collection("campaigns").add(docRef);
+    console.log("Updated successfully: ", docRef.id);
+    db.collection("campaigns-requests").where("campaignID", '==', cid).delete();
+    res.send(200, "Document written with ID: ", docRef.id);
+});
 
-// POSTS
+// get last 4 campaign posts
+const last = require('./routes/api/campaigns/last4campaigns');
+app.use('/api/campaignposts/lastfour', last);
 
-//get all blood needed posts
-const bloodReq = require('./routes/api/posts/bloodNeeded');
-app.use('/api/blood_needed_posts', bloodReq);
-
-
-// ORGANIZERS
-
-// get all organizers
-const viewOrgs = require('./routes/api/organizers/viewOrg');
-app.use('/api/organizers', viewOrgs);
-
-// insert a new organizer to the db
-const newOrg = require('./routes/api/organizers/insertNewOrg');
-app.use('/api/neworganizer', newOrg);
-
-// get organizer by id
-const orgId = require('./routes/api/organizers/orgById');
-app.use('/api/organizers/:uid', orgId);
-
-// get the next organizer id
-const nextOrg = require('./routes/api/organizers/nextOrgId');
-app.use('/api/organizers/nextid', nextOrg);
-
-
-// DONORS
-
-// get all donors
-const viewDonor = require('./routes/api/donor/viewDonors');
-app.use('/api/donors', viewDonor);
-
-// get donor by id
-const donId = require('./routes/api/donor/donorById');
-app.use('/api/donors/:uid', donId);
-
-// get the next donor id
-const nextDonId = require('./routes/api/donor/nextDonorId');
-app.use('/api/donors/nextid', nextDonId);
 
 // MAPS
-
-
-app.get('/api/bloodneededposts/lastfour', function (req, res) {
-    const bloodposts = [];
-    var lastID;
-    var tempID;
-    var nextID;
-
-    db.collection("posts").doc("blood_needed_posts").collection("blood_needed_posts").orderBy('publishedDateTime', "desc").limit(4).get().
-        then(snapshot => {
-            if (snapshot.empty) {
-                console.log('No matching documents.');
-                return;
-            }
-            snapshot.forEach(doc => {
-                var dataArray = doc.data();
-                bloodposts.push(dataArray)
-            });
-
-            // console.log("blood_need_posts: " + JSON.stringify(bloodposts))
-            res.send(JSON.stringify(bloodposts))
-        })
-        .catch(err => {
-            console.log('Error getting documents', err);
-        });
-});
-
-app.get('/api/campaignposts/lastfour', function (req, res) {
-    const campaignposts = [];
-    var lastID;
-    var tempID;
-    var nextID;
-
-    db.collection("posts").doc("campaign_posts").collection("campaign_posts").where('status', '==', 'accepted').orderBy('publishedDateTime', "desc").limit(4).get().
-        then(snapshot => {
-            if (snapshot.empty) {
-                console.log('No matching documents.');
-                return;
-            }
-            snapshot.forEach(doc => {
-                var dataArray = doc.data();
-                campaignposts.push(dataArray)
-            });
-
-            // console.log("blood_need_posts: " + JSON.stringify(bloodposts))
-            res.send(JSON.stringify(campaignposts))
-        })
-        .catch(err => {
-            console.log('Error getting documents', err);
-        });
-});
 
 // get past campaign markers in grey
 const greyMarkers = require('./routes/api/maps/greyMarkers');
@@ -180,3 +102,83 @@ app.use('/api/maps/greyMarkers', greyMarkers);
 // // get past campaign markers in yellow
 // const yellowMarkers = require('./routes/api/maps/yellowMarkers');
 // app.use('/api/maps/yellowMarkers', yellowMarkers);
+
+
+
+// POSTS
+
+//get all blood needed posts
+const bloodReq = require('./routes/api/posts/bloodNeeded');
+app.use('/api/blood_needed_posts', bloodReq);
+
+// get last 4 blood need posts
+const lastposts = require('./routes/api/posts/last4posts');
+app.use('/api/bloodneededposts/lastfour', lastposts);
+
+// ORGANIZERS
+
+// get all organizers
+const viewOrgs = require('./routes/api/organizers/viewOrg');
+app.use('/api/organizers', viewOrgs);
+
+// insert a new organizer to the db
+const newOrg = require('./routes/api/organizers/insertNewOrg');
+app.use('/api/neworganizer', newOrg);
+
+// get organizer by id
+// const orgId = require('./routes/api/organizers/orgById');
+// app.use('/api/organizers/:uid', orgId);
+
+app.get('/api/organizers/:uid', function (req, res) {
+    const uid = req.params.uid
+
+    // matches the uid with the given parameter
+    db.collection("users-organizer").where("uid", "==", uid).get()
+        .then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data());
+                res.send(JSON.stringify(doc.data()))
+            });
+        })
+        .catch(function (error) {
+            console.log("Error getting documents: ", error);
+        });
+});
+
+// get the next organizer id
+const nextOrg = require('./routes/api/organizers/nextOrgId');
+app.use('/api/organizers/nextid', nextOrg);
+
+
+// DONORS
+
+// get all donors
+const viewDonor = require('./routes/api/donor/viewDonors');
+app.use('/api/donors', viewDonor);
+
+// get donor by id
+// const donId = require('./routes/api/donor/donorById');
+// app.use('/api/donors/:uid', donId);
+
+// get donor by id
+app.get('/api/donors/:uid', function (req, res) {
+    const uid = req.params.uid
+
+    db.collection("users-donor").where("uid", "==", uid).get()
+        .then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data());
+                res.send(JSON.stringify(doc.data()))
+            });
+        })
+        .catch(function (error) {
+            console.log("Error getting documents: ", error);
+        });
+});
+
+
+// get the next donor id
+const nextDonId = require('./routes/api/donor/nextDonorId');
+app.use('/api/donors/nextid', nextDonId);
